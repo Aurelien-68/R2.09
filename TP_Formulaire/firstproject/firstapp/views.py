d9e9dda63b88a0b9b9a9be9f5e8bed9b4684279c
e2400575 <aurelienhenry9@gmail.com>
2025-04-04 12:01:00 +0200

    ff

--- TP_Formulaire/firstproject/firstapp/views.py
+++ TP_Formulaire/firstproject/firstapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'firstapp/index.html')
+
+def rep(request):
+    nom=request.POST["nom"]
+    return render(request,'firstapp/rep.html',{"nom":nom})
